0d78b2cb8fcf509346ccb14dc6cb8b7dea6134a4
Josiah Hunsinger <josiah.hunsinger@gmail.com>
2020-03-15 04:00:58 -0400

    Uses breadth first search to find optimal solution without stack overflows. Words for simple test example

--- block_slider.py
+++ block_slider.py
@@ -6,10 +6,15 @@ init_grid = np.array([[2, 1, 1, 3],
                       [4, 5, 5, 6],
                       [4, 7, 8, 6],
                       [9, 0, 0, 10]], dtype=np.int32)
+win = lambda g: g[4, 1]==1 and g[4, 2]==1 
+
+# BLOCKS = 2
+# init_grid = np.array([[1, 0],
+#                       [0, 2]], dtype=np.int32)
+# win = lambda g: g[1, 1]==1
 
 ROWS= len(init_grid)
 COLS= len(init_grid[0])
-win = lambda g: g[4, 1]==1 and g[4, 2]==1 
 
 RIGHT=0
 DOWN=1
@@ -42,39 +47,60 @@ def move(grid, block, direction):
                 new_grid[i, j] = grid[i, j]
     return new_grid
 
-def solve(grid, past_grid_hashes={}):
+def solve(init_grid):
     global BLOCKS
-    past_grid_hashes[ hash(str(grid)) ] = True
+    checked_grid_hashes = {}
+    current_depth = 0
     
-    # Can we win from any of the possible moves?
-    for block in range(1, BLOCKS+1):
-        for direction in range(4):
-            new_grid = move(grid, block, direction)
+    edge_solutions = [([], init_grid)] # A solution matches (move_hist, resulting_grid)
+
+    while True:
+        best_partial_solution_ind = 0
+        best_partial_solution_cost = None
+        for i in range(len(edge_solutions)):
+            partial_solution = edge_solutions[best_partial_solution_ind]
+            if best_partial_solution_cost == None or len(partial_solution[0]) < best_partial_solution_cost:
+                best_partial_solution_ind = i
+                best_partial_solution_cost = len(partial_solution[0])
+        if best_partial_solution_cost > current_depth:
+            current_depth = best_partial_solution_cost
+            print('Current depth:', current_depth, ', Edge nodes:',len(edge_solutions), end='\r')
+
+        move_hist, grid = edge_solutions[best_partial_solution_ind]
+        next_partial_solutions = []
+        for block in range(1, BLOCKS+1):
+            for direction in range(4):
+                new_grid = move(grid, block, direction)
+                
+                if new_grid is None: # Can't move that way
+                    continue
+                if hash(str(new_grid)) in checked_grid_hashes: # Already checked
+                    continue
+                
+                # Is this a win?
+                if win(new_grid):
+                    return move_hist + [(block, direction)]
+                
+                # Add next moves to list of moves to consider
+                checked_grid_hashes[ hash(str(new_grid)) ] = True
+                next_partial_solutions.append( (move_hist + [(block, direction)], new_grid) )
             
-            # If we can't move that way or we've been there before, don't try it
-            if new_grid is None:
-                continue
-            if hash(str(new_grid)) in past_grid_hashes:
-                continue
-            
-            # Did we win?
-            if win(new_grid):
-                # Tell the selves who remember where we've been of our glorious victory!
-                return [new_grid]
-            
-            # This move hasn't made us win, but it still could. Past it up the chain
-            solution = solve(new_grid, past_grid_hashes)
-            if solution is not None: 
-                # Someone up the chain figured it out :D
-                # Add our contribution before passing it down
-                return [new_grid] + solution
-     
+        # We apparently didn't find a win. Update edge_solutions
+        del edge_solutions[best_partial_solution_ind]
+        edge_solutions.extend( next_partial_solutions )
+
+
+
+
+        
+
     # We can't win from here :(
     return None
 
 result = solve(init_grid)
 print('MOVES:', len(result))
-for grid in result:
-    print(grid)
-    print('    |')
-    print('    V')
+print(init_grid)
+for move in result:
+    print('   |')
+    print('   V')
+    print(move)
